bd09b8e4c6f1f2286771e034b4cd955ef6aca9d9
Ece ERKCİ <ece6800@icloud.com>
2022-11-19 22:31:04 +0300

    javaÇalışmaları
    JAVA

---
+++ classesWithAttribute/src/productManager.java
@@ -0,0 +1,6 @@
+public class productManager {
+    public void Add(Product product){
+        System.out.println("ürün eklendi"+product.name);
+    }
+    public void Add2 (int id,String name,String description,int stockAmount ,double price,String renk,String kod )
+}
