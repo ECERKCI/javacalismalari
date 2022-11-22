2ba062044205695a66a6d38b0609c21a5369a034
Ece ERKCİ <ece6800@icloud.com>
2022-11-22 21:29:23 +0300

    java
    java

---
+++ packagesDemo/src/Main.java
@@ -0,0 +1,18 @@
+import Matematik.*;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner( System.in );
+
+        System.out.println("Adınız:");
+        String isim = scanner.nextLine();
+        System.out.println("Merhaba" + isim);
+
+    dortIslem dortIslem= new dortIslem();
+    dortIslem.topla(2,3);
+        Logaritma logaritma= new Logaritma();
+
+
+    }
+}
