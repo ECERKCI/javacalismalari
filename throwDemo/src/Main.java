427bdb107ddb40daf661917b3d2059a3649f2c36
Ece ERKCİ <ece6800@icloud.com>
2022-11-23 13:24:20 +0300

    java

---
+++ throwDemo/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+AccountManager manager= new AccountManager();
+System.out.println("Hesap = "+manager.getBalance());
+manager.deposit(100);
+        System.out.println("Hesap = "+manager.getBalance());
+        try {
+            manager.withdraw(90);
+        }catch (Exception exception){
+           System.out.println(exception.getMessage());
+        }
+        System.out.println("Hesap = "+manager.getBalance());
+        try {
+            manager.withdraw(90);
+        }catch (Exception exception){
+            System.out.println(exception.getMessage());
+        }
+        System.out.println("Hesap = "+manager.getBalance());
+        try {
+            manager.withdraw(20);
+        }catch (Exception exception){
+            System.out.println(exception.getMessage());
+        }
+        
+
+
+    }
+}
